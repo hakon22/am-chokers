0b60439b899d2de8948ddf26e826485a63d55466
Aleksey <hakon1@mail.ru>
2024-11-02 07:05:17 +0300

    tech: изменил интерфейс на тип

--- src/types/order/Order.ts
+++ src/types/order/Order.ts
@@ -2,4 +2,4 @@ import type { InitialState } from '@/types/InitialState';
 import type { OrderEntity } from '@server/db/entities/order.entity';
 import type { OmitBase } from '@/types/OmitBase';
 
-export interface OrderInterface extends OmitBase<OrderEntity>, InitialState {}
+export type OrderInterface = OmitBase<OrderEntity> & InitialState;
