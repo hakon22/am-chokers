f967cc556baeaa0ff08d9da9a990bc71cdc88265
Aleksey <hakon1@mail.ru>
2025-01-14 22:51:33 +0300

    feat: og теги

--- src/components/Helmet.tsx
+++ src/components/Helmet.tsx
@@ -10,7 +10,20 @@ export const Helmet = ({ title, description, image }: HelmetProps) => (
   <Head>
     <title>{title}</title>
     <meta name="description" content={description} />
-    {image && <meta name="image" content={image} />}
-    {typeof window !== 'undefined' && <link rel="canonical" href={window.location.href} />}
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
+    {image
+      ? <>
+        <meta name="image" content={`${process.env.NEXT_PUBLIC_PRODUCTION_HOST}${image}`} />
+        <meta property="og:image" content={`${process.env.NEXT_PUBLIC_PRODUCTION_HOST}${image}`} />
+      </>
+      : null}
+    {typeof window !== 'undefined'
+      ?
+      <>
+        <link rel="canonical" href={window.location.href} />
+        <meta property="og:url" content={window.location.href} />
+      </>
+      : null}
   </Head>
 );
