13bdd5f6ebb0e4e18d3ed6818242ad335700484b
Aleksey <hakon1@mail.ru>
2025-03-31 21:17:23 +0300

    feat: google и yandex метрики

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -93,6 +93,50 @@ const Init = (props: InitPropsInterface) => {
                       <link rel="apple-touch-icon" sizes="180x180" href={favicon180.src} />
                     </Head>
                     <Script 
+                      id="google-analytics"
+                      src="https://www.googletagmanager.com/gtag/js?id=G-P50BP1JPGM"
+                      async
+                      onLoad={() => {
+                        console.log('Google analytics script loaded');
+                      }}
+                      onError={(e) => {
+                        console.error('Error loading Google metrics script', e);
+                      }}
+                    />
+                    <Script
+                      id="gtag-init"
+                      strategy="afterInteractive"
+                      dangerouslySetInnerHTML={{
+                        __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-P50BP1JPGM');
+                    `,
+                      }}
+                    />
+                    <Script
+                      id="yandex-metrika"
+                      strategy="afterInteractive"
+                      dangerouslySetInnerHTML={{
+                        __html: `
+                        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                        m[i].l=1*new Date();
+                        for (var j = 0; j < document.scripts.length; j++) {
+                            if (document.scripts[j].src === r) { return; }
+                        }
+                        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+                        ym(100705426, "init", {
+                            clickmap:true,
+                            trackLinks:true,
+                            accurateTrackBounce:true
+                        });
+                    `,
+                      }}
+                    />
+                    <Script 
                       id="yandex-widget"
                       src="https://ndd-widget.landpro.site/widget.js"
                       strategy="lazyOnload"
