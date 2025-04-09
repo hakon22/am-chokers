cf50805a3a9e5816cd34f1616ad775e96aa3cbf0
Aleksey <hakon1@mail.ru>
2025-04-09 08:58:03 +0300

    fix: исправление отступа footer и доступа к заказу со стороны администратора

--- src/pages/admin/orders/[id].tsx
+++ src/pages/admin/orders/[id].tsx
@@ -4,41 +4,28 @@ import axios from 'axios';
 import { useContext, useEffect, useState } from 'react';
 
 import { routes } from '@/routes';
-import { UserRoleEnum } from '@server/types/user/enums/user.role.enum';
 import { BackButton } from '@/components/BackButton';
 import { Order as OrderComponent } from '@/components/profile/Order';
 import { useAppSelector } from '@/utilities/hooks';
 import { MobileContext, SubmitContext } from '@/components/Context';
 import { axiosErrorHandler } from '@/utilities/axiosErrorHandler';
-import { paramsIdSchema } from '@server/utilities/convertation.params';
 import type { OrderInterface } from '@/types/order/Order';
 import type { OrderResponseInterface } from '@/slices/orderSlice';
 import { Helmet } from '@/components/Helmet';
-import { Navigate } from '@/components/Navigate';
 
 export const getServerSideProps = async ({ params }: { params: { id: string; } }) => {
-  try {
-    const query = await paramsIdSchema.validate(params);
-
-    return {
-      props: { id: query.id },
-    };
-  } catch {
-    return {
-      redirect: {
-        permanent: false,
-        destination: routes.homePage,
-      },
-    };
-  }
+  const { id } = params;
+  return {
+    props: { id },
+  };
 };
 
 const Order = ({ id }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  console.log(id);
   const { t: tOrder } = useTranslation('translation', { keyPrefix: 'pages.profile.orders.order' });
   const { t: tToast } = useTranslation('translation', { keyPrefix: 'toast' });
 
   const { axiosAuth } = useAppSelector((state) => state.app);
-  const { role } = useAppSelector((state) => state.user);
 
   const { setIsSubmit } = useContext(SubmitContext);
   const { isMobile } = useContext(MobileContext);
@@ -48,7 +35,7 @@ const Order = ({ id }: InferGetServerSidePropsType<typeof getServerSideProps>) =
   const fetchOrder = async () => {
     try {
       setIsSubmit(true);
-      const { data } = await axios.get<OrderResponseInterface>(routes.crudOrder(id));
+      const { data } = await axios.get<OrderResponseInterface>(routes.crudOrder(+id));
       if (data.code === 1) {
         setOrder(data.order);
       }
@@ -64,7 +51,7 @@ const Order = ({ id }: InferGetServerSidePropsType<typeof getServerSideProps>) =
     }
   }, [axiosAuth]);
 
-  return role === UserRoleEnum.ADMIN ? (
+  return (
     <div className="d-flex flex-column mb-5 justify-content-center">
       <Helmet title={tOrder('title', { id })} description={tOrder('description')} />
       <h1 className="font-mr_hamiltoneg text-center fs-1 fw-bold mb-3 mb-xl-5" style={{ marginTop: isMobile ? '30%' : '12%' }}>{tOrder('title', { id })}</h1>
@@ -73,7 +60,7 @@ const Order = ({ id }: InferGetServerSidePropsType<typeof getServerSideProps>) =
       </div>
       {order && <OrderComponent order={order} orderId={order.id} />}
     </div>
-  ) : <Navigate to={routes.homePage} replace />;
+  );
 };
 
 export default Order;
